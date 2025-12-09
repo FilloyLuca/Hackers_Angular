f85e0e8f669fc6d93b9c7d9700b204ed42011217
FilloyLuca <filloylucapro@gmail.com>
2025-12-09 08:53:47 +0100

    component header footer et home

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Header } from './component/header/header';
+import { Footer } from './component/footer/footer';
+import { Home } from './component/home/home';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header,
+    Footer,
+    Home
   ],
   imports: [
     BrowserModule,
