9e7d8e096fc9245e32ccc64924c2d5e21521729b
FilloyLuca <filloylucapro@gmail.com>
2025-12-09 10:52:25 +0100

    lookup-ip.ts fait

--- src/app/service/lookup-ip.ts
+++ src/app/service/lookup-ip.ts
@@ -1,8 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LookupIp {
-  
+  constructor(private http: HttpClient) { }
+
+  public getGeoLocationIp(ip: string): Observable<any> {
+    return this.http.get(environment.apiBaseUrl + ip + '?output=json&access_key=' +
+      environment.keyAPI);
+  }
 }
