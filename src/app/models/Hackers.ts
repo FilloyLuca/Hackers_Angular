4d37d762a7b235bd6f4287c895ad659187c4b374
FilloyLuca <filloylucapro@gmail.com>
2025-12-09 10:38:00 +0100

    Hackers.ts et IHackers.ts fait

---
+++ src/app/models/Hackers.ts
@@ -0,0 +1,11 @@
+import { IHacker } from "./IHackers";
+export class Hacker implements IHacker {
+  constructor(
+  public ip: string,
+  public countryName: string,
+  public regionName: string,
+  public city: string,
+  public id?: string) {
+  // rien à faire de plus ici
+  }
+}
