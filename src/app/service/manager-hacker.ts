67de0ee38a4b89d490edb951d471d7a31f47b583
FilloyLuca <filloylucapro@gmail.com>
2025-12-11 10:29:35 +0100

    manager-hackers.ts modiffier avec ajout de getAllHackers()

--- src/app/service/manager-hacker.ts
+++ src/app/service/manager-hacker.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Hacker } from '../models/Hackers';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ManagerHacker {
-  
+
+  constructor() { }
+
+
+ /**
+  * Les hackers sont stockés localement côté client (localStorage)
+  * @renvoie la liste des Hackers
+  */
+  getAllHackers(): Hacker[] {
+    return JSON.parse(localStorage.getItem('badguys') || '[]');
+  }
+
 }
